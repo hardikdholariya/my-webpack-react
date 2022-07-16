d8e9787a1121e8e330da561948135a0aaf871f8d
--unset <--unset>
2022-07-16 22:30:15 +0530

    webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,11 +1,6 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-// const stylesHandler = isProduction
-//   ? MiniCssExtractPlugin.loader
-//   : "style-loader";
 
 module.exports = {
   entry: "./src/index.js",
@@ -19,9 +14,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "public/index.html",
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -33,20 +25,6 @@ module.exports = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
-
-/* module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-
-    config.plugins.push(new MiniCssExtractPlugin());
-  } else {
-    config.mode = "development";
-  }
-  return config;
-} */
