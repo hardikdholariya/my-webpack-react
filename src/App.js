f6a4aa0f122f0906ff1cd0d43a624c58e26b7c50
--unset <--unset>
2022-07-17 13:01:25 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
-import "./style.css";
-export default function App() {
-  console.log("hell");
+import logo from "./assets/images/logo.png";
+import "./assets/css/App.css";
+
+function App() {
   return (
-    <div>
-      <h1>h8sdfjsdklfj</h1>
-      <input type="text" />
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-white">React Webpack</h1>
+        <div className="main">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a className="App-link m-2" href="https://webpack.js.org/">
+          Learn Webpack
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
